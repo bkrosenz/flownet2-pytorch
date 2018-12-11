92228a930c7031fd95d8b1c399d22294ac6454d5
Ben Rosenzweig <bkrosenz@iu.edu>
2018-12-11 14:12:31 -0500

    fixed inference param

--- main.py
+++ main.py
@@ -155,10 +155,10 @@ if __name__ == '__main__':
             # block.log('Inference Input: {}'.format(' '.join([str([d for d in x.size()]) for x in inference_dataset[0][0]])))
             # block.log('Inference Targets: {}'.format(' '.join([str([d for d in x.size()]) for x in inference_dataset[0][1]])))
             # inference_loader = DataLoader(inference_dataset, batch_size=args.effective_inference_batch_size, shuffle=False, **inf_gpuargs)
-             inference_loader = DataLoader(
+            inference_loader = DataLoader(
                  datasets.ImagesFromFolder(args=args,is_cropped=False,root=args.inference_dataset_root,iext='jpg'),
                  batch_size=args.inference_batch_size,shuffle=False,**gpuargs
-                             )
+             )
     # Dynamically load model and loss class with parameters passed in via "--model_[param]=[value]" or "--loss_[param]=[value]" arguments
     with tools.TimerBlock("Building {} model".format(args.model)) as block:
         class ModelAndLoss(nn.Module):
