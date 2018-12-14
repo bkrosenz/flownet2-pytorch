ef3b48936b4ebc9a5909586a792c3fcf08646f80
Ben Rosenzweig <bkrosenz@iu.edu>
2018-12-13 20:59:02 -0500

    recursive

--- datasets.py
+++ datasets.py
@@ -28,7 +28,8 @@ class StaticCenterCrop(object):
         return img[(self.h-self.th)//2:(self.h+self.th)//2, (self.w-self.tw)//2:(self.w+self.tw)//2,:]
 
 class MpiSintel(data.Dataset):
-    def __init__(self, args, is_cropped = False, root = '', dstype = 'clean', replicates = 1):
+    def __init__(self, args, is_cropped = False,
+                 root = '', dstype = 'clean', replicates = 1):
         self.args = args
         self.is_cropped = is_cropped
         self.crop_size = args.crop_size
@@ -318,14 +319,17 @@ class ChairsSDHomTest(ChairsSDHom):
         super(ChairsSDHomTest, self).__init__(args, is_cropped = is_cropped, root = root, dstype = 'test', replicates = replicates)
 
 class ImagesFromFolder(data.Dataset):
-  def __init__(self, args, is_cropped, root = '/path/to/frames/only/folder', iext = 'png', replicates = 1):
+  def __init__(self, args, is_cropped,
+               root = '/path/to/frames/only/folder',
+               iext = 'png',
+               replicates = 1):
     self.args = args
     self.is_cropped = is_cropped
     self.crop_size = args.crop_size
     self.render_size = args.inference_size
     self.replicates = replicates
 
-    images = sorted( glob( join(root, '*.' + iext) ) )
+    images = sorted( glob( join(root, '*.' + iext), recursive=True ) )
     self.image_list = []
     for i in range(len(images)-1):
         im1 = images[i]
@@ -363,6 +367,117 @@ class ImagesFromFolder(data.Dataset):
 
   def __len__(self):
     return self.size * self.replicates
+
+
+class FramePairs(data.Dataset):
+    """hdf file of frame pairs from many sequences"""
+    def __init__(self, args, is_cropped = False, root = '',
+                 dstype = 'clean',
+                 
+                 replicates = 1):
+        self.args = args
+        self.is_cropped = is_cropped
+        self.crop_size = args.crop_size
+        self.render_size = args.inference_size
+        self.replicates = replicates
+
+        flow_root = join(root, 'flow')
+        image_root = join(root, dstype)
+
+        file_list = sorted(glob(join(flow_root, '*/*.flo')))
+
+        self.flow_list = []
+        self.image_list = []
+
+        for file in file_list:
+            if 'test' in file:
+                # print file
+                continue
+
+            fbase = file[len(flow_root)+1:]
+            fprefix = fbase[:-8]
+            fnum = int(fbase[-8:-4])
+
+            img1 = join(image_root, fprefix + "%04d"%(fnum+0) + '.png')
+            img2 = join(image_root, fprefix + "%04d"%(fnum+1) + '.png')
+
+            if not isfile(img1) or not isfile(img2) or not isfile(file):
+                continue
+
+            self.image_list += [[img1, img2]]
+            self.flow_list += [file]
+
+        self.size = len(self.image_list)
+
+        self.frame_size = frame_utils.read_gen(self.image_list[0][0]).shape
+
+        if (self.render_size[0] < 0) or (self.render_size[1] < 0) or (self.frame_size[0]%64) or (self.frame_size[1]%64):
+            self.render_size[0] = ( (self.frame_size[0])//64 ) * 64
+            self.render_size[1] = ( (self.frame_size[1])//64 ) * 64
+
+        args.inference_size = self.render_size
+
+        assert (len(self.image_list) == len(self.flow_list))
+
+    def __getitem__(self, index):
+
+        index = index % self.size
+
+        img1 = frame_utils.read_gen(self.image_list[index][0])
+        img2 = frame_utils.read_gen(self.image_list[index][1])
+
+        flow = frame_utils.read_gen(self.flow_list[index])
+
+        images = [img1, img2]
+        image_size = img1.shape[:2]
+
+        if self.is_cropped:
+            cropper = StaticRandomCrop(image_size, self.crop_size)
+        else:
+            cropper = StaticCenterCrop(image_size, self.render_size)
+        images = list(map(cropper, images))
+        flow = cropper(flow)
+
+        images = np.array(images).transpose(3,0,1,2)
+        flow = flow.transpose(2,0,1)
+
+        images = torch.from_numpy(images.astype(np.float32))
+        flow = torch.from_numpy(flow.astype(np.float32))
+
+        return [images], [flow]
+
+    def __len__(self):
+        return self.size * self.replicates
+
+class H5Dataset(data.Dataset):
+
+    def __init__(self, file_path, group, x, y):
+        super(H5Dataset, self).__init__()
+        h5_file = h5py.File(file_path,'r',libver='latest', swmr=True))[group]
+        self.data = h5_file.get(x)
+        self.target = h5_file.get(y)
+
+    def __getitem__(self, index):            
+        return (torch.from_numpy(self.data[index,...]).float(),
+                torch.from_numpy(self.target[index,...]).float())
+
+    def __len__(self):
+        return self.data.shape[0]
+
+class AviDataset(data.Dataset):
+
+    def __init__(self, file_path, group, x, y):
+        super(H5Dataset, self).__init__()
+        h5_file = h5py.File(file_path,'r',libver='latest', swmr=True))[group]
+        self.data = h5_file.get(x)
+        self.target = h5_file.get(y)
+
+    def __getitem__(self, index):            
+        return (torch.from_numpy(self.data[index,...]).float(),
+                torch.from_numpy(self.target[index,...]).float())
+
+    def __len__(self):
+        return self.data.shape[0]
 
 '''
 import argparse
